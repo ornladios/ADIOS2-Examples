263ece41b127e4ab3a77d42a6864cc62ec1185bf
Nick <nathompson7@protonmail.com>
2020-04-09 10:11:36 -0400

    Lorenz reader improvements.

--- source/cpp/lorenz_ode/lorenz_reader.cpp
+++ source/cpp/lorenz_ode/lorenz_reader.cpp
@@ -5,53 +5,86 @@
 #include <adios2.h>
 #include "lorenz.hpp"
 
-int main()
+void read_solution()
 {
     using Real = double;
     adios2::ADIOS adios;
     adios2::IO io = adios.DeclareIO("myio");
     adios2::Engine adios_engine = io.Open("lorenz.bp", adios2::Mode::Read);
     auto sigma_att = io.InquireAttribute<Real>("σ");
+    std::cout << "Simulation performed with ";
     if (sigma_att) {
         Real sigma = sigma_att.Data()[0];
-        std::cout << "σ = " << sigma << "\n";
+        std::cout << "σ = " << sigma << ", ";
     }
 
     auto beta_att = io.InquireAttribute<Real>("β");
     if (beta_att) {
         Real beta = beta_att.Data()[0];
-        std::cout << "β = " << beta << "\n";
+        std::cout << "β = " << beta << ", ";
     }
 
     auto rho_att = io.InquireAttribute<Real>("ρ");
     if (rho_att) {
         Real rho = rho_att.Data()[0];
-        std::cout << "ρ = " << rho << "\n";
+        std::cout << "and ρ = " << rho << ".\n";
     }
 
     auto interpretation_att = io.InquireAttribute<std::string>("interpretation");
     if (interpretation_att) {
         std::string interpretation = interpretation_att.Data()[0];
-        std::cout << "Interpretation: " << interpretation << "\n";
+        std::cout << "The data should be interpreted as a " << interpretation << ".\n";
     }
 
     auto absolute_error_att = io.InquireAttribute<Real>("‖û-u‖");
     if (absolute_error_att) {
-        std::cout << "Solutions computed with absolute error goal of " << absolute_error_att.Data()[0] << "\n";
+        std::cout << "Solutions were computed with an absolute error goal of " << absolute_error_att.Data()[0] << "\n";
     }
 
-    size_t paths = 2;
+    // Check that std::vector<std::array<Real, 7>> has the proper memory layout.
+    static_assert(sizeof(std::array<Real, 7>)==7*sizeof(Real),
+                  "The std::array on your system does not have the proper layout to be correctly deserialized in ADIOS2.");
+
+
     std::vector<std::array<Real, 7>> v;
-    for (size_t i = 0; i < paths; ++i) {
-        for (size_t j = 0; j < paths; ++j) {
-            for (size_t k = 0; k < paths; ++k) {
-                std::string variable = "u" + std::to_string(i) + std::to_string(j) + std::to_string(k);
-                auto state_variable = io.InquireVariable<Real>(variable);
-                if (state_variable) {
-                    std::cout << "We have state variable " << variable << "\n";
-                }
-            }
+    auto const & m = io.AvailableVariables();
+    for (auto const & [key, val] : io.AvailableVariables())
+    {
+        auto state_variable = io.InquireVariable<Real>(key);
+        if (!state_variable) {
+            std::cerr << "We do not have a state variable " << key << "\n";
+            continue;
         }
+        const auto shape = state_variable.Shape();
+        if (shape.size() != 1) {
+            std::cerr << "Expected 1D layout.\n";
+            continue;
+        }
+        if ((shape[0] % 7) != 0) {
+            std::cerr << "The size of a 7D array of structs must be a multiple of 7.\n";
+            continue;
+        }
+        size_t states = shape[0]/7;
+        if (v.capacity() < states) {
+            v.reserve(states);
+        }
+
+        double* d = v[0].data();
+        adios_engine.Get(key, d, adios2::Mode::Sync);
     }
     adios_engine.Close();
+}
+
+int main()
+{
+    try
+    {
+        read_solution();
+    }
+    catch (std::exception const & e)
+    {
+        std::cerr << "Caught exception reading Lorenz ODE solution: " << e.what() << "\n";
+        return 1;
+    }
+    return 0;
 }
