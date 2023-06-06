93cd22a97d7f9c9fd5a2685961d8c297c8d92d3a
anagainaru <ana.gainaru@gmail.com>
2023-06-06 09:21:40 -0400

    Add a Gray-Scott code using Kokkos View for the simulation data

---
+++ source/gpu/gray-scott-kokkos/restart.h
@@ -0,0 +1,14 @@
+#ifndef __RESTART_H__
+#define __RESTART_H__
+
+#include "gray-scott.h"
+
+#include <adios2.h>
+#include <mpi.h>
+
+void WriteCkpt(MPI_Comm comm, const int step, const Settings &settings,
+               const GrayScott &sim, adios2::IO io);
+int ReadRestart(MPI_Comm comm, const Settings &settings, GrayScott &sim,
+                adios2::IO io);
+
+#endif
